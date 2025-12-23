f551c54514fa06e97fc48995c2dc8edc148b135b
Kim Hyunjin <ehgmlgg1216@gmail.com>
2025-12-23 14:01:54 +0900

    BebeNote 깃 연동

---
+++ BebeNote/src/main/java/com/itwillbs/BebeNoteApplication.java
@@ -0,0 +1,13 @@
+package com.itwillbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebeNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BebeNoteApplication.class, args);
+	}
+
+}
