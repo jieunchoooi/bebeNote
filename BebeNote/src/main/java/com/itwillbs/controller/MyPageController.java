ceed34266bcd36d80c133715b71de86d6296e996
jieun <vvvnogreen@gmail.com>
2025-12-26 11:42:19 +0900

    마이페이지 프론트 구현 및 매핑

--- BebeNote/src/main/java/com/itwillbs/controller/MyPageController.java
+++ BebeNote/src/main/java/com/itwillbs/controller/MyPageController.java
@@ -19,5 +19,23 @@ public class MyPageController {
 			return "myPage/info";
 	}
 	
+	@GetMapping("/myHospital")
+	public String myHospital() {
+		System.out.println("MyPageController myHospital()");
+			return "myPage/myHospital";
+	}
+	
+	@GetMapping("/payHistory")
+	public String payHistory() {
+		System.out.println("MyPageController payHistory()");
+			return "myPage/payHistory";
+	}
+	
+	@GetMapping("/payControl")
+	public String payControl() {
+		System.out.println("MyPageController payControl()");
+			return "myPage/payControl";
+	}
+	
 
 }
