df8919ceaf1832db458ddba89c4e6140ac4a75d3
jieun <vvvnogreen@gmail.com>
2026-01-28 14:11:57 +0900

    자녀정보페이지 수정

--- BebeNote/src/main/java/com/itwillbs/controller/MyPageController.java
+++ BebeNote/src/main/java/com/itwillbs/controller/MyPageController.java
@@ -29,87 +29,87 @@ import lombok.RequiredArgsConstructor;
 @Log
 @RequestMapping("/myPage/*")
 public class MyPageController {
-	
-	private final MyPageService myPageService;
-	private final ChildrenService childrenService;
-	private final PaymentService paymentService; 
-	
-	@GetMapping("/info")
-	public String info(HttpSession session, Model model) {
-		System.out.println("MyPageController info()");
-		
-		String userId = SecurityContextHolder.getContext().getAuthentication().getName();
-		
-		List<Children> children = childrenService.findAllByUserId(userId);
-		if (children == null) {
+   
+   private final MyPageService myPageService;
+   private final ChildrenService childrenService;
+   private final PaymentService paymentService; 
+   
+   @GetMapping("/info")
+   public String info(HttpSession session, Model model) {
+      System.out.println("MyPageController info()");
+      
+      String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+      
+      List<Children> children = childrenService.findAllByUserId(userId);
+      if (children == null) {
             model.addAttribute("noChild", true);
             return "myPage/info";
         }
-		
-		Long childId = children.get(0).getChild_id();
-	    List<ChildVaccine> records = myPageService.findChildRecords(childId);
-	    
-	    // vaccine_id + "_" + dose_id를 key로 하는 Map 생성
-	    Map<String, ChildVaccine> vaccineRecords = new HashMap<>();
-	    for (ChildVaccine record : records) {
-	        String key = record.getVaccine_id() + "_" + record.getDose_id();
-	        vaccineRecords.put(key, record);
-	    }
-		
-		model.addAttribute("children", children);
-		model.addAttribute("vaccines", myPageService.findAllVaccines());
-	    model.addAttribute("doses", myPageService.findAllDoses());
-	    model.addAttribute("vaccineRecords", vaccineRecords);
-		
-			return "myPage/info";
-	}
-	
-	@PostMapping("/saveVaccine")
-	@ResponseBody
-	public Map<String, Object> saveVaccine(@RequestBody ChildVaccineVO vo) {
-		System.out.println("MyPageController saveVaccine()");
-		
-		 Long childVaccineId = myPageService.saveVaccine(vo);
-		 Map<String, Object> result = new HashMap<>();
-		 result.put("childVaccineId", childVaccineId);
-		 
-		 return result;
-		 
-	}
-	
-	@PostMapping("/deleteVaccine")
-	@ResponseBody
-	public void deleteVaccine(@RequestBody ChildVaccineVO vo) {
-		System.out.println("MyPageController deleteVaccine()");
-		
-		 myPageService.deleteVaccine(vo.getChild_vaccine_id());
-		 
-	}
-	
-	@GetMapping("/myHospital")
-	public String myHospital() {
-		System.out.println("MyPageController myHospital()");
-			return "myPage/myHospital";
-	}
-	
-	@GetMapping("/payHistory")
-	public String payHistory() {
-		System.out.println("MyPageController payHistory()");
-			return "myPage/payHistory";
-	}
-	
-	@GetMapping("/payControl")
-	public String payControl(Model model) {
-		System.out.println("MyPageController payControl()");
-		
-		// 로그인한 사용자 ID 가져오기
-		String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
-		
-		// 베베페이 잔액 조회
-		int balance = paymentService.getBebepayBalance(user_id);
-		model.addAttribute("balance", balance);
-			return "myPage/payControl";
-	}
-	
+      
+      Long childId = children.get(0).getChild_id();
+       List<ChildVaccine> records = myPageService.findChildRecords(childId);
+       
+       // vaccine_id + "_" + dose_id를 key로 하는 Map 생성
+       Map<String, ChildVaccine> vaccineRecords = new HashMap<>();
+       for (ChildVaccine record : records) {
+           String key = record.getVaccine_id() + "_" + record.getDose_id();
+           vaccineRecords.put(key, record);
+       }
+      
+      model.addAttribute("children", children);
+      model.addAttribute("vaccines", myPageService.findAllVaccines());
+       model.addAttribute("doses", myPageService.findAllDoses());
+       model.addAttribute("vaccineRecords", vaccineRecords);
+      
+         return "myPage/info";
+   }
+   
+   @PostMapping("/saveVaccine")
+   @ResponseBody
+   public Map<String, Object> saveVaccine(@RequestBody ChildVaccineVO vo) {
+      System.out.println("MyPageController saveVaccine()");
+      
+       Long childVaccineId = myPageService.saveVaccine(vo);
+       Map<String, Object> result = new HashMap<>();
+       result.put("childVaccineId", childVaccineId);
+       
+       return result;
+       
+   }
+   
+   @PostMapping("/deleteVaccine")
+   @ResponseBody
+   public void deleteVaccine(@RequestBody ChildVaccineVO vo) {
+      System.out.println("MyPageController deleteVaccine()");
+      
+       myPageService.deleteVaccine(vo.getChild_vaccine_id());
+       
+   }
+   
+   @GetMapping("/myHospital")
+   public String myHospital() {
+      System.out.println("MyPageController myHospital()");
+         return "myPage/myHospital";
+   }
+   
+   @GetMapping("/payHistory")
+   public String payHistory() {
+      System.out.println("MyPageController payHistory()");
+         return "myPage/payHistory";
+   }
+   
+   @GetMapping("/payControl")
+   public String payControl(Model model) {
+      System.out.println("MyPageController payControl()");
+      
+      // 로그인한 사용자 ID 가져오기
+      String user_id = SecurityContextHolder.getContext().getAuthentication().getName();
+      
+      // 베베페이 잔액 조회
+      int balance = paymentService.getBebepayBalance(user_id);
+      model.addAttribute("balance", balance);
+         return "myPage/payControl";
+   }
+   
 
 }
